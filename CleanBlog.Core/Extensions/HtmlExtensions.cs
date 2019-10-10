075214658ec2fac3a2a8215122a9555cadbe1a91
Paul Seal <prjseal@gmail.com>
2019-10-10 13:37:57 +0100

    - end of episode 6 where we added the grid

---
+++ CleanBlog.Core/Extensions/HtmlExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+
+namespace CleanBlog.Core.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static IHtmlString GetGridHtml(this HtmlHelper html, IPublishedContent contentItem, string framework, string propertyAlias, bool fluidImages)
+        {
+            if (fluidImages)
+            {
+                return html.Raw(html.GetGridHtml(contentItem, framework, propertyAlias).ToString().Replace("<img ", "<img class=\"img-fluid\" "));
+            }
+            else
+            {
+                return html.GetGridHtml(contentItem, propertyAlias, framework);
+            }
+        }
+    }
+}
