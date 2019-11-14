77fa074378afcab684cf1f4bda67e01d008961f1
Paul Seal <prjseal@gmail.com>
2019-11-14 17:27:19 +0000

    - end of episode 9

---
+++ CleanBlog.Core/Helpers/QueryStringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+
+namespace CleanBlog.Core.Helpers
+{
+    public static class QueryStringHelper
+    {
+        public static int GetIntFromQueryString(HttpRequestBase request, string key, int fallbackValue = 0)
+        {
+            var stringValue = request.QueryString[key];
+            if (stringValue != null && !string.IsNullOrWhiteSpace(stringValue) &&
+                int.TryParse(stringValue, out var numericValue))
+            {
+                return numericValue;
+            }
+
+            return fallbackValue;
+        }
+    }
+}
