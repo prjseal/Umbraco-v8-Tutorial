7ba014e58aaccafecde3770e7be9724a77fca3bf
Paul Seal <prjseal@gmail.com>
2020-01-09 14:41:59 +0000

    - end of episode 14

---
+++ CleanBlog.Core/Composing/TreeNodeRenderingComposer.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Services;
+using Umbraco.Web.Trees;
+
+namespace CleanBlog.Core.Composing
+{
+
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class TreeNodeRenderingComposer : ComponentComposer<TreeNodeRenderingComponent>, IUserComposer
+    { }
+
+    public class TreeNodeRenderingComponent : IComponent
+    {
+        private readonly IContentTypeService _contentTypeService;
+
+        public TreeNodeRenderingComponent(IContentTypeService contentTypeService)
+        {
+            _contentTypeService = contentTypeService;
+        }
+
+        public void Initialize()
+        {
+            TreeControllerBase.TreeNodesRendering += TreeControllerBase_TreeNodesRendering;
+        }
+
+        public void Terminate()
+        { }
+
+        private void TreeControllerBase_TreeNodesRendering(TreeControllerBase sender, TreeNodesRenderingEventArgs e)
+        {
+            if (sender.TreeAlias == "documentTypes")
+            {
+                var contentTypes = _contentTypeService.GetAll().ToDictionary(c => c.Id);
+                foreach (var node in e.Nodes)
+                {
+                    if (node.NodeType == "documentTypes")
+                    {
+                        if (int.TryParse(node.Id.ToString(), out var nodeId) && nodeId > 0)
+                        {
+                            var contentType = contentTypes[nodeId];
+                            node.Icon = contentType.Icon;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CleanBlog.Web/scripts/pleasedontgo.js
@@ -0,0 +1,32 @@
+﻿var PDG = PDG || {
+    isOriginalTitle: true,
+    originalTitle: '',
+    leaveMessage: '😥 Please dont go!!!',
+    init: function() {
+        PDG.originalTitle = document.title;
+        PDG.listeners();
+    },
+    listeners: function() {
+        document.addEventListener('mouseout', function(e) {
+            if(PDG.isOriginalTitle)
+            {
+                PDG.updateTitle();
+            }
+        }, false);
+
+        document.addEventListener('mousemove', function(e) {
+            if(!PDG.isOriginalTitle)
+            {
+                PDG.resetTitle();
+            }
+        }, false);
+    },
+    resetTitle: function() {
+        document.title = PDG.originalTitle;
+        PDG.isOriginalTitle = true;
+    },
+    updateTitle: function() {
+        document.title = PDG.leaveMessage + ' | ' + PDG.originalTitle;
+        PDG.isOriginalTitle = false;
+    }
+};
